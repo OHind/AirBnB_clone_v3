582ba6da13c4f243af8d8cd242b42e1eb0c7ce89
OHind <ouchani.hind@gmail.com>
2024-01-08 23:08:18 +0100

    3.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Blueprint for API"""
-import Flask from flask
-import Blueprint from flask
+"""Init file for views module"""
 
-
+from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-import * from api.v1.views.index
-import * from api.v1.views.state
-import * from api.v1.views.cities
-import * from api.v1.views.amenities
-import * from api.v1.views.users
-import * from api.v1.views.places
-import * from api.v1.views.places_reviews
-import * from api.v1.views.places_amenities
+from api.v1.views import *
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 """
-index.py
+This module contains endpoint(route) status
 """
-import app_views from api.v1.views
-import jsonify from flask
-import storage from models
-import Amenity from models.amenity
-import City from models.city
-import Place from models.place
-import Review from models.review
-import State from models.state
-import User from models.user
+
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from flask import Flask
+from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Status of the API"""
+    """
+    Returns a JSON status
+    """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', methods=['GET'])
-    def count_objects():
-    """Retrives the number of each object by type"""
-    return jsonify({"amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    })
+def count_objs():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
